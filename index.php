3807462c5277c35c0b3f8fc6c82431f404df2bec
pmourey <philippe@mourey.com>
2018-01-17 18:59:24 +0100

    Page d'authentification par mot de passe à l'application NodeJS (FPS Autosquare)

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: philippemourey
+ * Date: 17/01/2018
+ * Time: 18:04
+ */
+
+if ( isset($_GET['pwd']) )
+{
+    if ($_GET['pwd'] == "afpa")
+    {
+        header("Location: http://2.15.32.3:3000");
+        exit();
+    }
+    else
+        echo "Mot de passe incorrect!";
+}
+else {
+?>
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Code d'accès au super FPS</title>
+    </head>
+    <body>
+    <h1>Page d'accès au super FPS</h1>
+    <form method="get" action="index.php">
+        <label>Mot de passe
+            <input type=password name="pwd">
+        </label>
+        <input type="submit" value="OK">
+    </form>
+
+    </body>
+    </html>
+
+<?php
+    }
+?>
+
+
+
